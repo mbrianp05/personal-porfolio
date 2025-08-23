c70b323f7f8678c8bdfed04565787d4e1b837da4
Brian <mbrianp05@gmail.com>
2025-08-23 13:35:23 -0400

    Improve error handling

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const projectNames = [
   "dip-web-design",
 ];
 const projectStars = document.querySelectorAll(".stars");
+const starIconTemplate = document.querySelector("#star-svg-icon");
 
 projectNames.forEach(async (projectName, index) => {
-  const response = await fetch(
-    `https://api.github.com/repos/mbrianp05/${projectName}`
-  );
-  const data = await response.json();
+  const starClone = starIconTemplate.content.cloneNode(true);
 
-  projectStars[index].textContent += data.stargazers_count;
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/mbrianp05/${projectName}`
+    );
+
+    const { stargazers_count } = await response.json();
+
+    if (stargazers_count !== undefined) {
+      projectStars[index].textContent = stargazers_count;
+    } else projectStars[index].remove();
+  } finally {
+    projectStars[index].prepend(starClone);
+  }
 });
